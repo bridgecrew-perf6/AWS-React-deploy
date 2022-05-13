8f43f2ae2e8c78090a0c16c74fda13c333459f96
srbobhate187 <bobhatesr187@gmail.com>
2022-05-13 23:22:53 +0530

    updated text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const[value, setValue] = useState("")
+  const [results, setResults] = useState([])
+// G8FAa7wNnLIAX9nHufC7sef2iqyE5tU91ADnye6tw6Q
+  const fetchPics = () =>{
+    fetch(`https://api.unsplash.com/search/photos?client_id=G8FAa7wNnLIAX9nHufC7sef2iqyE5tU91ADnye6tw6Q&query=${value}&orientation=squarish`)
+    .then(res=>res.json())
+    .then(data=>{
+      // 
+      setResults(data.results)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         How to  host react app on AWS.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        <div>
+        
+          <input type="text" name="search" className="round" value ={value} onChange = {(e) =>setValue (e.target.value)} />
+          <button onClick = {()=>fetchPics()}>search</button>
+        </div>
+        <div className = "gallery">
+          {
+            results.map((item)=>{
+              return <img className ="pics" key = {item.id} src = {item.urls.regular}/>
+            })
+          }
+          
+        </div>
+      </div>
+    </main>
   );
 }
 
